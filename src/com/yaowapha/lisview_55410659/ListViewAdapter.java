e25baec1fe7e4e2a8f19210ddfbb083bcc0e4b6b
Yaowapha <saisin.2536@gmail.com>
2014-09-03 17:13:33 +0700

    listview

---
+++ src/com/yaowapha/lisview_55410659/ListViewAdapter.java
@@ -0,0 +1,18 @@
+package com.yaowapha.lisview_55410659;
+
+import android.graphics.drawable.Drawable;
+
+public class ListViewAdapter {
+	public class List_items { private Drawable drawable; 
+	private String title;
+	public Drawable getDrawable() {
+		return drawable; 
+	} 
+	public void setDrawable(Drawable drawable) { this.drawable = drawable; }
+	public String getTitle() { return title; } public void setTitle(String title) { this.title = title; } }
+	
+
+	
+	
+
+}
